34b6f108d2eb8e318d201dbeb0c760689ce0e645
n1rjal <nirjalpaudel54312@gmail.com>
2020-06-05 16:49:03 +0545

    debugto false

--- WareHouse/settings.py
+++ WareHouse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ervw1yt0b!g%fr$8jtovxg+t7l^l=g!$1jo718(fe^+wcm3uw+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
